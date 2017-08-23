87cbaf0bd8b21d8620ad644313e059b085fb051a
Duc Nguyen <ducnd412@gmail.com>
2017-08-23 13:26:15 +0700

    Init.

---
+++ swagger.go
@@ -0,0 +1,75 @@
+package swagger
+
+import (
+	"log"
+	"text/template"
+	"github.com/GeertJohan/go.rice"
+	"net/http"
+	"strings"
+	"fmt"
+)
+
+type Config struct {
+	SwaggerUrl        string
+	OauthClientId     string
+	OauthClientSecret string
+	OauthScope        string
+}
+type SwaggerHandler struct {
+	config        *Config
+	staticHandler http.Handler
+	template      *template.Template
+}
+
+func New(config *Config) http.Handler {
+	conf := rice.Config{
+		LocateOrder: []rice.LocateMethod{rice.LocateEmbedded, rice.LocateAppended, rice.LocateFS},
+	}
+	static, err := conf.FindBox("static")
+	if err != nil {
+		log.Fatalf("error opening static: %s\n", err)
+	}
+	viewBox, err := rice.FindBox("view")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	indexContent, err := viewBox.String("index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// parse and execute the template
+	indexTemplate, err := template.New("message").Parse(indexContent)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &SwaggerHandler{
+		config:        config,
+		staticHandler: http.FileServer(static.HTTPBox()),
+		template:      indexTemplate,
+	}
+
+}
+
+func (s *SwaggerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	upath := r.URL.Path
+	basePath := upath[0:strings.LastIndex(upath, "/")]
+	fmt.Println(upath, basePath)
+	if strings.HasSuffix(upath, ".png") || strings.HasSuffix(upath, ".css") || strings.HasSuffix(upath, ".js") {
+		if p := strings.TrimPrefix(r.URL.Path, basePath); len(p) < len(r.URL.Path) {
+			r.URL.Path = p
+			s.staticHandler.ServeHTTP(w, r)
+		}
+		return
+	}
+	data := map[string]interface{}{
+		"BasePath": basePath,
+		"Config":   s.config,
+	}
+	error := s.template.Execute(w, data)
+	if error != nil {
+		log.Fatal(error)
+	}
+}
---
+++ swagger_test.go
@@ -0,0 +1,27 @@
+package swagger
+
+import (
+	"testing"
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func Test(t *testing.T) {
+	swaggerHandler := New(&Config{
+		SwaggerUrl:        "http://petstore.swagger.io/v2/swagger.json",
+		OauthClientId:     "test client id",
+		OauthClientSecret: "test client secret",
+		OauthScope:        "scope",
+	})
+
+	http.Handle("/swagger/", swaggerHandler)
+	go func() {
+		fmt.Println("Serving files on :8082, press ctrl-C to exit")
+		err := http.ListenAndServe(":8082", nil)
+		if err != nil {
+			log.Fatalf("error serving files: %v", err)
+		}
+	}()
+	select {}
+}
