268eea3717b042af0570c88d6c22a82d71217d27
Duc Nguyen <ducnd412@gmail.com>
2017-08-30 09:59:02 +0700

    Move to swagger 2.

--- swagger.go
+++ swagger.go
@@ -5,19 +5,24 @@ import (
 	"text/template"
 	"github.com/GeertJohan/go.rice"
 	"net/http"
+	"fmt"
 	"strings"
 )
 
 type Config struct {
+	BasePath          string
 	SwaggerUrl        string
 	OauthClientId     string
 	OauthClientSecret string
 	OauthScope        string
+	Realm             string
+	AppName           string
 }
 type SwaggerHandler struct {
 	config        *Config
 	staticHandler http.Handler
 	template      *template.Template
+	static        *rice.Box
 }
 
 func New(config *Config) http.Handler {
@@ -45,6 +50,7 @@ func New(config *Config) http.Handler {
 
 	return &SwaggerHandler{
 		config:        config,
+		static:        static,
 		staticHandler: http.FileServer(static.HTTPBox()),
 		template:      indexTemplate,
 	}
@@ -53,21 +59,31 @@ func New(config *Config) http.Handler {
 
 func (s *SwaggerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-	upath := r.URL.Path
-	basePath := upath[0:strings.LastIndex(upath, "/")]
-	if strings.HasSuffix(upath, ".png") || strings.HasSuffix(upath, ".css") || strings.HasSuffix(upath, ".js") {
-		if p := strings.TrimPrefix(r.URL.Path, basePath); len(p) < len(r.URL.Path) {
-			r.URL.Path = p
+	urlPath := r.URL.Path
+	prefix := strings.TrimPrefix(r.URL.Path, s.config.BasePath)
+	fmt.Println(urlPath, prefix)
+
+	if file, err := s.static.Open(prefix); err == nil {
+		if f, e := file.Stat(); e == nil && !f.IsDir() {
+			r.URL.Path = prefix
 			s.staticHandler.ServeHTTP(w, r)
+			return
 		}
-		return
 	}
-	data := map[string]interface{}{
-		"BasePath": basePath,
-		"Config":   s.config,
-	}
-	error := s.template.Execute(w, data)
+	error := s.template.Execute(w, s.config)
 	if error != nil {
 		log.Fatal(error)
 	}
+
+	//if strings.HasSuffix(path, ".png") || strings.HasSuffix(path, ".css") || strings.HasSuffix(path, ".js") {
+	//	if p := strings.TrimPrefix(r.URL.Path, s.config.BasePath); len(p) < len(r.URL.Path) {
+	//		r.URL.Path = p
+	//		s.staticHandler.ServeHTTP(w, r)
+	//	}
+	//	return
+	//}
+	//error := s.template.Execute(w, s.config)
+	//if error != nil {
+	//	log.Fatal(error)
+	//}
 }
